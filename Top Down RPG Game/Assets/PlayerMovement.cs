7d0c8945bdc15463e3d9ed28d245b975a589cbd6
marie-bisbo <marie@brainlabsdigital.com>
2020-06-03 12:50:18 +0100

    Add character with movement and animations

---
+++ Top Down RPG Game/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        SetPlayerDirection();
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement.normalized * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void SetPlayerDirection ()
+    {
+        Vector3 playerDirection = transform.localScale;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerDirection.x = -1;
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerDirection.x = 1;
+        }
+        transform.localScale =playerDirection;
+    }
+}
